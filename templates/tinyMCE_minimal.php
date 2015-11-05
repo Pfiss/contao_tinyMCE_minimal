40305a66c9e6b881a0204e6ee793cf4e00f32b07
Friedrich Pfitzmann <friedrich@pfiss.com>
2015-11-05 16:23:39 +0100

    Added license and readme for the repo

--- templates/tinyMCE_minimal.php
+++ templates/tinyMCE_minimal.php
@@ -2,8 +2,7 @@
 
 /**
  *
- * h2first - simple module for Contao 3 to change the ordering for the headlines
- * to not start with a h1
+ * tinyMCE_minimal - simple module for Contao 3 to minimize the functionality for tinyMCE to its core
  *
  * Copyright (C) 2015  Friedrich Pfitzmann Software Solutions
  *
@@ -42,39 +41,7 @@ if ($GLOBALS['TL_CONFIG']['useRTE']):
             templates: [
                 <?php echo Backend::getTinyTemplates(); ?>
             ],
-            //style_formats_merge: true,
-            style_formats: [
-            /*    {title: "Headers", items: [
-                    {title: "Header 2", format: "h2"},
-                    {title: "Header 3", format: "h3"},
-                    {title: "Header 4", format: "h4"},
-                    {title: "Header 5", format: "h5"},
-                    {title: "Header 6", format: "h6"},
-                    {title: "Header 1", format: "h1"}
-                ]},
-                {title: "Inline", items: [
-                    {title: "Bold", icon: "bold", format: "bold"},
-                    {title: "Italic", icon: "italic", format: "italic"},
-                    {title: "Underline", icon: "underline", format: "underline"},
-                    {title: "Strikethrough", icon: "strikethrough", format: "strikethrough"},
-                    {title: "Superscript", icon: "superscript", format: "superscript"},
-                    {title: "Subscript", icon: "subscript", format: "subscript"},
-                    {title: "Code", icon: "code", format: "code"}
-                ]},
-                {title: "Blocks", items: [
-                    {title: "Paragraph", format: "p"},
-                    {title: "Blockquote", format: "blockquote"},
-                    {title: "Div", format: "div"},
-                    {title: "Pre", format: "pre"}
-                ]},
-                {title: "Alignment", items: [
-                    {title: "Left", icon: "alignleft", format: "alignleft"},
-                    {title: "Center", icon: "aligncenter", format: "aligncenter"},
-                    {title: "Right", icon: "alignright", format: "alignright"},
-                    {title: "Justify", icon: "alignjustify", format: "alignjustify"}
-                ]}*/
-            ],
-           //plugins: "autosave charmap code fullscreen image importcss link lists paste searchreplace tabfocus table template visualblocks",
+            style_formats: [],
             plugins: "autosave fullscreen link lists paste searchreplace tabfocus template visualblocks",
             external_plugins: {"wordcount": "<?php echo TL_PATH; ?>/system/modules/pfiss-minimal_tiny_mce/assets/tinyMCE/plugins/wordcount/plugin.min.js"},
             browser_spellcheck: true,
